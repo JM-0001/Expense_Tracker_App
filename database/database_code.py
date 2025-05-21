01277af81818b4f43f5fd2866adfd7758406efb6
Juan Miguel Sanchez <sanchezjuanmiguel95@gmail.com>
2025-05-21 23:18:57 +0800

    kms

--- database/database_code.py
+++ database/database_code.py
@@ -14,6 +14,25 @@ user_id TEXT PRIMARY KEY,
 last_name TEXT NOT NULL,
 first_name TEXT)''')
 
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Categories (
+category_type TEXT PRIMARY KEY,
+category_description TEXT NOT NULL)''')
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Expenses (
+expense_id INT PRIMARY KEY,
+amount TEXT NOT NULL,
+category TEXT NOT NULL,
+date DATE NOT NULL,
+time TIME NOT NULL
+description TEXT,
+user TEXT NOT NULL)''')
+
+data = [("ass", "cheeks"), ("cock", "sucker")]
+
+cursor.executemany('''INSERT INTO UserAuthentication (username, password) VALUES (?, ?)''', data)
+
 con.commit()
 
 # for row in cursor.execute("SELECT * FROM UserAuthentication"):
